521f27f89ebc7a1d01436450779b16f005e1799e
isahua16 <isaelhuard@gmail.com>
2023-03-28 18:47:22 -0600

    reading a cookie

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ function create_cookie()
 {
     Cookies.set(`button_clicked`, `yes`);
 }
-
 let button_one = document.querySelector(`#btn_1`);
 button_one.addEventListener(`click`, create_cookie);
+
+if(!Cookies.get(`button_clicked`))
+{
+    button_one.insertAdjacentHTML(`beforebegin`, `<h1>Please Click the button!</h1>`);
+}
+else
+{
+    button_one.insertAdjacentHTML(`beforebegin`, `<h1>Congrats on baking a cookie!</h1>`);
+}
