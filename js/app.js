363cc5f2625ce40b7eb45b5a7e49fd6fda7e7acc
isahua16 <isaelhuard@gmail.com>
2023-03-28 19:13:43 -0600

    removing cookies

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
-function create_cookie()
+function create_cookie(event)
 {
     Cookies.set(`button_clicked`, `yes`);
 }
+
+function delete_cookie(event)
+{
+    Cookies.remove(`button_clicked`);
+}
+
 let button_one = document.querySelector(`#btn_1`);
+let button_two = document.querySelector(`#btn_2`);
+
 button_one.addEventListener(`click`, create_cookie);
+button_two.addEventListener(`click`, delete_cookie);
 
 if(!Cookies.get(`button_clicked`))
 {
